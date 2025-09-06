c2b7b1224a50b89db5f6ffb7e30acc610a3bde8b
nimalpranav <premaavk@gmail.com>
2025-09-06 16:47:50 +0530

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login_view, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path('', views.home, name='home'),
 ]
+
